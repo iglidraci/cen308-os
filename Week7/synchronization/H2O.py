86f3cb2b11c437aab51d391987b77dd61a07c81a
iglidraci <iglidraci98@gmail.com>
2022-04-27 08:39:43 +0200

    h2o molecule

--- Week7/synchronization/H2O.py
+++ Week7/synchronization/H2O.py
@@ -1,19 +1,26 @@
-from threading import Semaphore, Barrier
+from threading import Semaphore
 
 
 class H2O:
     def __init__(self):
-        self.elements = []
-        self.h_semaphore = Semaphore(2)
-        self.o_semaphore = Semaphore(1)
-        self.b = Barrier(3)
+        self.nr_h = 0
+        self.nr_o = 0
+        self.h_semaphore = Semaphore(1)
+        self.o_semaphore = Semaphore(0)
 
     def hydrogen(self) -> None:
         with self.h_semaphore:
-            self.b.wait()
             print("H", end='')
+            self.nr_h += 1
+            if self.nr_h == 2:
+                self.nr_o = 0
+                self.o_semaphore.release()
+            else:
+                self.h_semaphore.release()
 
     def oxygen(self) -> None:
         with self.o_semaphore:
-            self.b.wait()
             print("O", end='')
+            if self.nr_o == 1:
+                self.nr_h = 0
+                self.h_semaphore.release()
